5f956f73cca1a4ee86859954d5260aa97f4ad939
Sergei Berezhnoi <berezhnoi.serg@gmail.com>
2018-04-19 19:54:53 +0300

    first anagram

---
+++ src/Anagram/Test.java
@@ -0,0 +1,10 @@
+package Anagram;
+import java.util.*;
+public class Test {
+	
+	public static void main(String[] args) {
+		StringBuffer buffer = new StringBuffer("#he&lo %w$or!ld");
+		buffer.reverse();
+		System.out.println(buffer);
+	}
+}
