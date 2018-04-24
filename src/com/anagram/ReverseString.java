3c97b5693bb1e33072587b6570512ceadb4c9f99
Sergei Berezhnoi <berezhnoi.serg@gmail.com>
2018-04-24 23:37:44 +0300

    my tokenizer

--- src/com/anagram/ReverseString.java
+++ src/com/anagram/ReverseString.java
@@ -1,31 +1,32 @@
 package com.anagram;
 
-
 import java.util.StringTokenizer;
 import java.util.*;
 import java.lang.*;
 import java.io.*;
- class STDemo {
+
+class ReverseString {
 	static String line = "#he&lo @%w$o,r!ld.";
 	static String key;
 	static String val;
+
 	public static void main(String[] args) {
 		StringTokenizer theWords = new StringTokenizer(line, "@.");
 		StringBuilder line1 = new StringBuilder();
 		StringBuilder line2 = new StringBuilder();
+
 		while (theWords.hasMoreTokens()) {
 			key = theWords.nextToken();
 			val = theWords.nextToken();
-			// Выводим лексимы в консоль
-			System.out.println(key + "" + val);
 		}
+
 		line1.append(key);
 		line1 = line1.reverse();
 		line2.append(val);
 		line2 = line2.reverse();
-		
+
 		System.out.println(line1 + " " + line2);
-		
+
 	}
 
 }
