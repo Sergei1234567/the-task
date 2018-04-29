654f6a5b03b8e23f489817a037cce6bee3c70e0d
Sergei Berezhnoi <berezhnoi.serg@gmail.com>
2018-04-29 22:12:32 +0300

    my anagram

--- src/com/anagram/ReverseString.java
+++ src/com/anagram/ReverseString.java
@@ -5,24 +5,19 @@ import java.util.StringTokenizer;
 class ReverseString {
 
 	public static void main(String[] args) {
-		String token1 = "";
-		String token2 = "";
+		String anagram = "";
 		String sentence = "#he&lo @%w$o,r!ld.";
 		String delim = "@.";
 
 		StringTokenizer tokenazer = new StringTokenizer(sentence, delim);
 		StringBuilder sentence1 = new StringBuilder();
-		StringBuilder sentence2 = new StringBuilder();
 
 		while (tokenazer.hasMoreTokens()) {
 
-			token1 = tokenazer.nextToken();
-			token2 = tokenazer.nextToken();
+			anagram = tokenazer.nextToken();
+			sentence1.append(anagram);
+			sentence1 = sentence1.reverse();
+			System.out.print(sentence1 + " ");
 		}
-		sentence1.append(token1);
-		sentence1 = sentence1.reverse();
-		sentence2.append(token2);
-		sentence2 = sentence2.reverse();
-		System.out.println(sentence1 + "\n " + sentence2);
 	}
 }
