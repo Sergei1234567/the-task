a3290c0b8045823f1404690ab8197d9a424241d1
Sergei Berezhnoi <berezhnoi.serg@gmail.com>
2018-05-07 21:55:01 +0300

    my anagram

--- src/com/anagram/TestReverseString.java
+++ src/com/anagram/TestReverseString.java
@@ -3,7 +3,7 @@ package com.anagram;
 public class TestReverseString {
 	public static void main(String[] args) {
 		ReverseString reverseString = new ReverseString();
-		String Anagram = reverseString.toAnagram("#he&lo @%w$o,rld.", "#&@%$,. ");
-		System.out.println(Anagram);
+		String anagram = reverseString.toAnagram("#he&lo @%w$o,rld.", "#&@%$,. ");
+		System.out.println(anagram);
 	}
 }
